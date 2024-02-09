02bfc71f31c894dece51195ea08c819c293d1de6
Chris Mcintosh <chris_mcintosh@outlook.com.au>
2024-02-09 14:20:33 +1000

    Initial commit

---
+++ src/main/java/org/mcralph/userdemothymeleaf/UserDemoThymeleafApplication.java
@@ -0,0 +1,13 @@
+package org.mcralph.userdemothymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDemoThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserDemoThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mcralph/userdemothymeleaf/UserDemoThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mcralph.userdemothymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDemoThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
